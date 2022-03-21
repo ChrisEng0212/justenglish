418f53b57ab9ed98354ef11e4d07247cceec0ad1
ChrisEng0212 <cjx02121981@gmail.com>
2022-03-21 13:08:23 +0800

    href

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 import gunicorn
+import werkzeug
+
 
 app = Flask(__name__)
 
 print('running')
-@app.route('/')
-def index():
+
+@app.route ("/<string:page>", methods=['GET','POST'])
+def index(page):
    return render_template('index.html')
 
 if __name__ == '__main__':
